a6e327e6065c9fe8ea64fad2d41d36ead6d4c0ea
Zekai Koç <zekaikoc@gmail.com>
2023-12-07 21:22:07 +0100

    done.

--- Advent_of_Code/Day_01/trebuchet.js
+++ Advent_of_Code/Day_01/trebuchet.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const readline = require("readline");
 
 // Replace 'file.txt' with the path to your text file
-const filePath = "example.txt";
+const filePath = "input.txt";
 
 // Create a readable stream from the file
 const fileStream = fs.createReadStream(filePath);
@@ -13,9 +13,31 @@ const rl = readline.createInterface({
    crlfDelay: Infinity, // To recognize all instances of CR LF ('\r\n') as a single line break
 });
 
-const createNumberArray = (line) => {
-   return line.filter((character) => !isNaN(character));
+const createNumberArray = (singleLine) => {
+   return singleLine.filter((character) => !isNaN(character));
 };
+
+const createTwoDigitNumber = (arrayOfNumbers) => {
+   const twoDigit = [];
+
+   if (arrayOfNumbers.length >= 2) {
+      twoDigit.push(arrayOfNumbers[0]);
+      twoDigit.push(arrayOfNumbers[arrayOfNumbers.length - 1]);
+   }
+
+   if (arrayOfNumbers.length === 1) {
+      twoDigit.push(arrayOfNumbers[0]);
+      twoDigit.push(arrayOfNumbers[0]);
+   }
+
+   if (arrayOfNumbers.length === 0) {
+      twoDigit.push(0);
+   }
+
+   return twoDigit.join("");
+};
+
+let total = 0;
 
 // Read the file line by line
 rl.on("line", (line) => {
@@ -23,11 +45,16 @@ rl.on("line", (line) => {
    // console.log("Line:", line);
    // const newLineArray = line.split("");
    // console.log("newLineArray:", newLineArray);
-   const numbers = createNumberArray(line.split(""));
-   console.log(numbers);
+   const numbersArray = createNumberArray(line.split(""));
+   console.log(numbersArray);
+
+   const twoDigitNumber = createTwoDigitNumber(numbersArray);
+   console.log(twoDigitNumber);
+   total += parseInt(twoDigitNumber);
 });
 
 // Handle end of file
 rl.on("close", () => {
+   console.log("total: ", total);
    console.log("End of file");
 });
