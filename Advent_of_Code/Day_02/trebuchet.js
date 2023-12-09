b4394243764d2a898e282871c717aca15e2cabdb
Zekai Koç <zekaikoc@gmail.com>
2023-12-09 14:06:30 +0100

    trying different approaches.

--- Advent_of_Code/Day_02/trebuchet.js
+++ Advent_of_Code/Day_02/trebuchet.js
@@ -26,32 +26,67 @@ const digitsInLetters = [
    "nine",
 ];
 
+const threeLetters = ["one", "two", "six"];
+const fourLetters = ["four", "five", "nine"];
+const fiveLetters = ["three", "seven", "eight"];
+
 const numbersWithPositions = [];
 
 const parseSingleLine = (singleLine) => {
    const numbersArray = singleLine.split("");
-   console.log("numbersArray: ", numbersArray);
-
-   for (let i = 0; i < numbersArray.length; i++) {
+   // console.log("numbersArray: ", numbersArray);
+   // splice ile sifirdan baslayarak pars et...
+   const newDigits = [];
+   for (let i = 0; i < numbersArray.length; ) {
       if (!isNaN(numbersArray[i])) {
-         console.log("=> ", numbersArray[i]);
-         numbersWithPositions.push({
-            number: numbersArray[i],
-            position: i,
-         });
+         console.log("NUM ", numbersArray[i]);
+         newDigits.push(numbersArray[i]);
+         i++;
+      } else {
+         const tempString = singleLine.slice(3);
+         console.log(tempString);
+
+         console.log("NaN ", numbersArray[i]);
+         i++;
+         // once 3 karakterlik bir buffer al ve icinde sayi var mi diye kontrol et.
+         // yoksa 4 karakterlik bir buffer
+         // 5 karakterlik buffer
       }
    }
+   console.log(newDigits);
 };
 
 // Read the file line by line
 rl.on("line", (line) => {
-   console.log("Line:", line);
+   // console.log("Line:", line);
    parseSingleLine(line);
+   console.log("-------------");
 });
 
 // Handle end of file
 rl.on("close", () => {
-   console.log("total: ", total);
-   console.log("numbersWithPositions: ", numbersWithPositions);
+   // console.log("total: ", total);
+   // console.log("numbersWithPositions: ", numbersWithPositions);
    console.log("End of file");
 });
+
+// const parseSingleLine = (singleLine) => {
+//    const numbersArray = singleLine.split("");
+//    // console.log("numbersArray: ", numbersArray);
+
+//    for (let i = 0; i < numbersArray.length; i++) {
+//       if (!isNaN(numbersArray[i])) {
+//          console.log("NUM ", numbersArray[i]);
+//          numbersWithPositions.push({
+//             number: numbersArray[i],
+//             position: i,
+//          });
+//       } else {
+//          console.log("NaN ", numbersArray[i]);
+//          numbersWithPositions.push({
+//             number: "NaN",
+//             position: i,
+//          });
+//       }
+//    }
+// };
