4689c165a6633e7738cacdc6e1c481e35da35650
Zekai Koç <zekaikoc@gmail.com>
2023-12-09 22:36:39 +0100

    bug fixing.

--- Advent_of_Code/Day_02/trebuchet.js
+++ Advent_of_Code/Day_02/trebuchet.js
@@ -1,10 +1,9 @@
-const { execSync } = require("child_process");
-
 const { subscribe } = require("diagnostics_channel");
 const fs = require("fs");
 const readline = require("readline");
 
 const filePath = "example.txt";
+// const filePath = "input.txt";
 
 const fileStream = fs.createReadStream(filePath);
 
@@ -14,48 +13,24 @@ const rl = readline.createInterface({
 });
 
 let total = 0;
-const digitsInLetters = [
-   "one",
-   "two",
-   "three",
-   "four",
-   "five",
-   "six",
-   "seven",
-   "eight",
-   "nine",
-];
 
-const threeLetters = ["one", "two", "six"];
-const fourLetters = ["four", "five", "nine"];
-const fiveLetters = ["three", "seven", "eight"];
+const createTwoDigitNumber = (arrayOfNumbers) => {
+   const twoDigit = [];
+   if (arrayOfNumbers.length >= 2) {
+      twoDigit.push(arrayOfNumbers[0]);
+      twoDigit.push(arrayOfNumbers[arrayOfNumbers.length - 1]);
+   }
+   if (arrayOfNumbers.length === 1) {
+      twoDigit.push(arrayOfNumbers[0]);
+      twoDigit.push(arrayOfNumbers[0]);
+   }
+   if (arrayOfNumbers.length === 0) {
+      twoDigit.push(0);
+   }
+   return twoDigit.join("");
+};
 
-const numbersWithPositions = [];
-
-// const numbersArray = singleLine.split("");
-// // console.log("numbersArray: ", numbersArray);
-// // splice ile sifirdan baslayarak pars et...
-// const newDigits = [];
-// for (let i = 0; i < numbersArray.length; ) {
-//    if (!isNaN(numbersArray[i])) {
-//       console.log("NUM ", numbersArray[i]);
-//       newDigits.push(numbersArray[i]);
-//       i++;
-//    } else {
-//       const tempString = singleLine.slice(3);
-//       console.log(tempString);
-
-//       console.log("NaN ", numbersArray[i]);
-//       i++;
-//       // once 3 karakterlik bir buffer al ve icinde sayi var mi diye kontrol et.
-//       // yoksa 4 karakterlik bir buffer
-//       // 5 karakterlik buffer
-//    }
-// }
-// console.log(newDigits);
 const parseSingleLine = (singleLine) => {
-   console.log("singleLine:", singleLine);
-   // two1nine
    const newDigits = [];
    for (let i = 0; i < singleLine.length; ) {
       if (!isNaN(singleLine[i])) {
@@ -63,10 +38,8 @@ const parseSingleLine = (singleLine) => {
          i++;
       } else {
          const tempSubStr = singleLine.substring(i, i + 5);
-         console.log("tempSubStr ", tempSubStr);
-
+         // console.log("tempSubStr ", tempSubStr);
          let fastForward = 1;
-
          if (tempSubStr.includes("one")) {
             newDigits.push(1);
             fastForward = 3;
@@ -79,7 +52,6 @@ const parseSingleLine = (singleLine) => {
             newDigits.push(6);
             fastForward = 3;
          }
-
          if (tempSubStr.includes("four")) {
             newDigits.push(4);
             fastForward = 4;
@@ -92,7 +64,6 @@ const parseSingleLine = (singleLine) => {
             newDigits.push(9);
             fastForward = 4;
          }
-
          if (tempSubStr.includes("three")) {
             newDigits.push(3);
             fastForward = 5;
@@ -107,42 +78,54 @@ const parseSingleLine = (singleLine) => {
          }
          i += fastForward;
       }
-      console.log(newDigits);
-      sleep(1000);
+      // console.log(newDigits);
+      // sleep(100);
    }
-   console.log(newDigits);
+   // console.log(newDigits);
+   return newDigits;
 };
-
-// switch (tempSubStr) {
-//    case tempSubStr.includes("one"):
-//       newDigits.push(1);
-//       fastForward = 3;
-//       break;
-//    case tempSubStr.includes("two"):
-//       newDigits.push(1);
-//       fastForward = 3;
-//       break;
-//    case tempSubStr.includes("six"):
-//       newDigits.push(1);
-//       fastForward = 3;
-//       break;
-//    default:
-//       break;
-// }
 
 // Read the file line by line
 rl.on("line", (line) => {
-   // console.log("Line:", line);
-   parseSingleLine(line);
-   // console.log("-------------");
+   // console.log(line);
+   const numArray = parseSingleLine(line);
+   // console.log(numArray);
+   const twoDigitNumber = createTwoDigitNumber(numArray);
+   // console.log(twoDigitNumber);
+   total += parseInt(twoDigitNumber);
+
+   console.log(line, numArray, twoDigitNumber, total);
 });
 
 // Handle end of file
 rl.on("close", () => {
    // console.log("total: ", total);
    // console.log("numbersWithPositions: ", numbersWithPositions);
+   console.log("total: ", total);
+
    console.log("End of file");
 });
+
+const sleep = (millis) => {
+   var stop = new Date().getTime();
+   while (new Date().getTime() < stop + millis) {}
+};
+
+// const digitsInLetters = [
+//    "one",
+//    "two",
+//    "three",
+//    "four",
+//    "five",
+//    "six",
+//    "seven",
+//    "eight",
+//    "nine",
+// ];
+
+// const threeLetters = ["one", "two", "six"];
+// const fourLetters = ["four", "five", "nine"];
+// const fiveLetters = ["three", "seven", "eight"];
 
 // const parseSingleLine = (singleLine) => {
 //    const numbersArray = singleLine.split("");
@@ -165,7 +148,19 @@ rl.on("close", () => {
 //    }
 // };
 
-const sleep = (millis) => {
-   var stop = new Date().getTime();
-   while (new Date().getTime() < stop + millis) {}
-};
+// switch (tempSubStr) {
+//    case tempSubStr.includes("one"):
+//       newDigits.push(1);
+//       fastForward = 3;
+//       break;
+//    case tempSubStr.includes("two"):
+//       newDigits.push(1);
+//       fastForward = 3;
+//       break;
+//    case tempSubStr.includes("six"):
+//       newDigits.push(1);
+//       fastForward = 3;
+//       break;
+//    default:
+//       break;
+// }
