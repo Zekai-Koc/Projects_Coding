d4071eda0a46ff591fb7311a4bd148d48475f980
Zekai Koç <zekaikoc@gmail.com>
2023-12-10 15:55:55 +0100

    finished for a single line.

--- Advent_of_Code/Day_02/Cube_Conundrum.js
+++ Advent_of_Code/Day_02/Cube_Conundrum.js
@@ -10,19 +10,42 @@ const rl = readline.createInterface({
    crlfDelay: Infinity,
 });
 
+const gameStats = [];
+
 rl.on("line", (line) => {
-   handleSingleLine(line);
+   const singleGameStats = handleSingleLine(line);
+   console.log(singleGameStats);
+   gameStats.push(singleGameStats);
 });
 
 const handleSingleLine = (singleLine) => {
-   console.log("singleLine: ", singleLine);
+   const [Red, Green, Blue] = [[], [], []];
+   const gameStats = {};
+   // console.log("singleLine: ", singleLine);
    const seperateGameId = singleLine.split(":");
-   console.log("seperateGameId[1]:", seperateGameId[1]);
+   // console.log("seperateGameId[1]:", seperateGameId[1]);
    const separateSets = seperateGameId[1].split(";");
-   console.log("separateSets: ", separateSets);
-
+   // console.log("separateSets: ", separateSets);
    separateSets.forEach((element) => {
       const separateColors = element.split(",");
-      console.log("separateColors: ", separateColors);
+      // console.log("separateColors: ", separateColors);
+      separateColors.forEach((element) => {
+         const separateColor = element.split(",")[0].trim();
+         // console.log("separateColor: ", separateColor);
+         const finalSeparation = separateColor.split(" ");
+         // console.log("finalSeparation: ", finalSeparation);
+         if (finalSeparation[1] === "red") Red.push(+finalSeparation[0]);
+         if (finalSeparation[1] === "green") Green.push(+finalSeparation[0]);
+         if (finalSeparation[1] === "blue") Blue.push(+finalSeparation[0]);
+      });
    });
+   // console.log({ Red }, { Green }, { Blue });
+   // console.log(Math.max(...Red));
+   // console.log(Math.max(...Green));
+   // console.log(Math.max(...Blue));
+   gameStats.gameId = seperateGameId[0].trim();
+   gameStats.Red = Math.max(...Red);
+   gameStats.Green = Math.max(...Green);
+   gameStats.Blue = Math.max(...Blue);
+   return gameStats;
 };
