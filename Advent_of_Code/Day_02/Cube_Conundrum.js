3bac89b9cf9ffc699be85b27951fc363a87e08c0
Zekai Koç <zekaikoc@gmail.com>
2023-12-10 15:28:54 +0100

    single line divided into elements.

--- Advent_of_Code/Day_02/Cube_Conundrum.js
+++ Advent_of_Code/Day_02/Cube_Conundrum.js
@@ -7,13 +7,22 @@ const fileStream = fs.createReadStream(filePath);
 
 const rl = readline.createInterface({
    input: fileStream,
-   crlfDelay: Infinity, // To recognize all instances of CR LF ('\r\n') as a single line break
+   crlfDelay: Infinity,
 });
 
 rl.on("line", (line) => {
-   console.log("Line:", line);
+   handleSingleLine(line);
 });
 
-rl.on("close", () => {
-   console.log("End of file");
-});
+const handleSingleLine = (singleLine) => {
+   console.log("singleLine: ", singleLine);
+   const seperateGameId = singleLine.split(":");
+   console.log("seperateGameId[1]:", seperateGameId[1]);
+   const separateSets = seperateGameId[1].split(";");
+   console.log("separateSets: ", separateSets);
+
+   separateSets.forEach((element) => {
+      const separateColors = element.split(",");
+      console.log("separateColors: ", separateColors);
+   });
+};
