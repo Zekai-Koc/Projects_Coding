7f07838accb2620a0f9ec50661980881e973e721
Zekai Koç <zekaikoc@gmail.com>
2023-12-11 14:57:33 +0100

    checking left and right.

--- Advent_of_Code/Day_03/Part_1/Gear_Ratios.js
+++ Advent_of_Code/Day_03/Part_1/Gear_Ratios.js
@@ -14,23 +14,58 @@ const matrix = createMatrix("example.txt");
 const row = 4;
 const col = 3;
 
-console.log(matrix[row][col]);
+// console.log(matrix[row][col]);
 
 const checkLeft = (row, col) => {
-   return matrix[row][col - 1] !== ".";
+   // return matrix[row][col - 1] !== undefined || isNaN(matrix[row][col - 1]);
+   return (
+      matrix[row][col - 1] === undefined ||
+      (isNaN(matrix[row][col - 1]) && matrix[row][col - 1] !== ".")
+   );
 };
 
 const checkRight = (row, col) => {
-   return matrix[row][col + 1] !== ".";
+   // return matrix[row][col + 1] !== ".";
+   return (
+      matrix[row][col + 1] === undefined ||
+      (isNaN(matrix[row][col + 1]) && matrix[row][col + 1] !== ".")
+   );
 };
 
-console.log(checkLeft(row, col));
-console.log(checkRight(row, col));
+// console.log(checkLeft(row, col));
+// console.log(checkRight(row, col));
 
-const readRow = (singleRow) => {
-   for (let i = 0; i < singleRow.length; i++) {
-      console.log(singleRow[i]);
+const readRow = (rowNum) => {
+   console.log(matrix[rowNum]);
+   for (let i = 0; i < matrix[rowNum].length; i++) {
+      // console.log("=> ", singleRow[i]);
+      let number = "";
+      let colIndex = [];
+      while (!isNaN(matrix[rowNum][i])) {
+         number += matrix[rowNum][i];
+         colIndex.push(i);
+         i++;
+      }
+
+      if (colIndex.length > 0) {
+         console.log("number: ", number);
+         console.log("colIndex: ", colIndex);
+
+         colIndex.forEach((element) => {
+            // console.log(checkLeft(rowNum, element));
+            console.log(
+               "checkRight: ",
+               matrix[rowNum][element],
+               checkRight(rowNum, element)
+            );
+            console.log(
+               "checkLeft: ",
+               matrix[rowNum][element],
+               checkLeft(rowNum, element)
+            );
+         });
+      }
    }
 };
 
-readRow(matrix[0]);
+readRow(0);
